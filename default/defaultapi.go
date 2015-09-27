9e52fb790c521197f940df85bd6fa3f03f416678
Frank Bille <github@frankbille.dk>
2015-09-27 22:35:30 +0200

    Fix the routing of the URL's to the different modules.
    It didn’t route correctly to the different modules, and within the
    modules. That is fixed now, and further simplified the dispatch, so it
    only handles the production routes. When developing locally, just use
    the individual host:port for each module.

---
+++ default/defaultapi.go
@@ -0,0 +1,17 @@
+package context
+
+import (
+	gin "github.com/gamescores/gin"
+  http "net/http"
+)
+
+func init() {
+  r := gin.New()
+
+  api := r.Group("/api")
+	api.GET("/", func(c *gin.Context) {
+		c.String(200, "HELLO")
+	})
+
+  http.Handle("/", r);
+}
